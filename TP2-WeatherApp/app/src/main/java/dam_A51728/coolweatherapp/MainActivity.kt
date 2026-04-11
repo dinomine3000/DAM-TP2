f7a943fdb027efa22a6a2c97aabf0727597fb5ba
dinomine3000 <67598019+dinomine3000@users.noreply.github.com>
2026-04-11 11:42:19 +0100

    made UI and copied some base code

--- TP2-WeatherApp/app/src/main/java/dam_A51728/coolweatherapp/MainActivity.kt
+++ TP2-WeatherApp/app/src/main/java/dam_A51728/coolweatherapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package dam_A51728.coolweatherapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,10 +12,90 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val day = false
+        when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> {
+                if (day) {
+                    setTheme(R.style.Theme_Day)
+                } else {
+                    setTheme(R.style.Theme_Night)
+                }
+            }
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                if (day) {
+                    setTheme(R.style.Theme_Day_Land)
+                } else {
+                    setTheme(R.style.Theme_Night_Land)
+                }
+            }
+        }
+        setContentView(R.layout.activity_main)
     }
+}
+
+data class WeatherData(
+    var latitude: String,
+    var longitude: String,
+    var timezons: String,
+    var current_weather: CurrentWeather,
+    var hourly: Hourly
+)
+
+data class CurrentWeather(
+    var temperature: Float,
+    var windspeed: Float,
+    var winddirection: Int,
+    var weathercode: Int,
+    var time: String
+)
+
+data class Hourly(
+    var time: ArrayList<String>,
+    var temperature_2m: ArrayList<Float>,
+    var weathercode: ArrayList<Int>,
+    var pressure_msl: ArrayList<Double>
+)
+
+enum class WMO_WeatherCode(val code: Int, val image: String) {
+    CLEAR_SKY(0, "clear_"),
+    MAINLY_CLEAR(1, "mostly_clear_"),
+    PARTLY_CLOUDY(2, "partly_cloudy_"),
+    OVERCAST(3, "cloudy"),
+    FOG(45, "fog"),
+    DEPOSITING_RIME_FOG(48, "fog"),
+    DRIZZLE_LIGHT(51, "drizzle"),
+    DRIZZLE_MODERATE(53, "drizzle"),
+    DRIZZLE_DENSE(55, "drizzle"),
+    FREEZING_DRIZZLE_LIGHT(56, "freezing_drizzle"),
+    FREEZING_DRIZZLE_DENSE(57, "freezing_drizzle"),
+    RAIN_SLIGHT(61, "rain_light"),
+    RAIN_MODERATE(63, "rain"),
+    RAIN_HEAVY(65, "rain_heavy"),
+    FREEZING_RAIN_LIGHT(66, "freezing_rain_light"),
+    FREEZING_RAIN_HEAVY(67, "freezing_rain_heavy"),
+    SNOW_FALL_SLIGHT(71, "snow_light"),
+    SNOW_FALL_MODERATE(73, "snow"),
+    SNOW_FALL_HEAVY(75, "snow_heavy"),
+    SNOW_GRAINS(77, "snow"),
+    RAIN_SHOWERS_SLIGHT(80, "rain_light"),
+    RAIN_SHOWERS_MODERATE(81, "rain"),
+    RAIN_SHOWERS_VIOLENT(82, "rain_heavy"),
+    SNOW_SHOWERS_SLIGHT(85, "snow_light"),
+    SNOW_SHOWERS_HEAVY(86, "snow_heavy"),
+    THUNDERSTORM_SLIGHT_MODERATE(95, "tstorm"),
+    THUNDERSTORM_HAIL_SLIGHT(96, "tstorm"),
+    THUNDERSTORM_HAIL_HEAVY(99, "tstorm")
+}
+
+fun getWeatherCodeMap(): Map<Int, WMO_WeatherCode>{
+    var weatherMap = HashMap<Int, WMO_WeatherCode>()
+    WMO_WeatherCode.values().forEach {
+        weatherMap.put(it.code, it)
+    }
+    return weatherMap
 }
